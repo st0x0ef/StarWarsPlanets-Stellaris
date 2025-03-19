ad92d20af121258fdf9d120b8d72a9930fd4b567
st0x0ef <gabriel.gaudreault3@gmail.com>
2025-03-19 13:36:18 -0400

    textures update

--- common/src/main/java/com/st0x0ef/swplanets/common/menu/BlasterUpgraderMenu.java
+++ common/src/main/java/com/st0x0ef/swplanets/common/menu/BlasterUpgraderMenu.java
@@ -1,7 +1,6 @@
 package com.st0x0ef.swplanets.common.menu;
 
 
-import com.st0x0ef.stellaris.common.menus.BaseContainer;
 import com.st0x0ef.stellaris.common.menus.slot.ResultSlot;
 import com.st0x0ef.swplanets.common.registry.MenusRegistry;
 import net.minecraft.network.FriendlyByteBuf;
@@ -9,18 +8,24 @@ import net.minecraft.world.Container;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
-public class BlasterUpgraderMenu extends BaseContainer {
+public class BlasterUpgraderMenu extends AbstractContainerMenu {
     private final Container container;
 
     public BlasterUpgraderMenu(int id, Inventory inventory, FriendlyByteBuf buf) {
         this(id, inventory, new SimpleContainer(3));
     }
     public BlasterUpgraderMenu(int id, Inventory inventory, Container container) {
-        super(MenusRegistry.BLASTER_UPGRADER_MENU.get(), id, 3, inventory, 26);
+        super(MenusRegistry.BLASTER_UPGRADER_MENU.get(), id);
 
         this.container = container;
+
+        addPlayerHotbar(inventory, 177);
+        addPlayerInventory(inventory, 35);
 
         addSlots();
     }
@@ -34,5 +39,63 @@ public class BlasterUpgraderMenu extends BaseContainer {
     @Override
     public boolean stillValid(Player player) {
         return this.container.stillValid(player);
+    }
+
+    private static final int HOTBAR_SLOT_COUNT = 9;
+    private static final int PLAYER_INVENTORY_ROW_COUNT = 3;
+    private static final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
+    private static final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_COLUMN_COUNT * PLAYER_INVENTORY_ROW_COUNT;
+    private static final int VANILLA_SLOT_COUNT = HOTBAR_SLOT_COUNT + PLAYER_INVENTORY_SLOT_COUNT - 1;
+    private static final int VANILLA_FIRST_SLOT_INDEX = 0;
+    private static final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
+
+    @Override
+    public @NotNull ItemStack quickMoveStack(Player playerIn, int index) {
+        Slot sourceSlot = slots.get(index);
+        if (!sourceSlot.hasItem()) return ItemStack.EMPTY;
+        ItemStack sourceStack = sourceSlot.getItem();
+        ItemStack copyOfSourceStack = sourceStack.copy();
+
+        int TE_INVENTORY_SLOT_COUNT = 3;
+        if (index < VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT) {
+            // This is a vanilla container slot so merge the stack into the tile inventory
+            if (!moveItemStackTo(sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX, TE_INVENTORY_FIRST_SLOT_INDEX
+                    + TE_INVENTORY_SLOT_COUNT, false)) {
+                return ItemStack.EMPTY;  // EMPTY_ITEM
+            }
+        } else if (index < TE_INVENTORY_FIRST_SLOT_INDEX + TE_INVENTORY_SLOT_COUNT) {
+            // This is a TE slot so merge the stack into the players inventory
+            if (!moveItemStackTo(sourceStack, VANILLA_FIRST_SLOT_INDEX, VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT, false)) {
+                return ItemStack.EMPTY;
+            }
+        } else {
+            System.out.println("Invalid slotIndex:" + index);
+            return ItemStack.EMPTY;
+        }
+        // If stack size == 0 (the entire stack was moved) set slot contents to null
+        if (sourceStack.getCount() == 0) {
+            sourceSlot.set(ItemStack.EMPTY);
+        } else {
+            sourceSlot.setChanged();
+        }
+        sourceSlot.onTake(playerIn, sourceStack);
+        return copyOfSourceStack;
+    }
+
+    public void addPlayerHotbar(Inventory playerInventory, int y) {
+        int j;
+        for(j = 0; j < 9; ++j) {
+            this.addSlot(new Slot(playerInventory, j, 11 + j * 18, y));
+        }
+    }
+
+    public void addPlayerInventory(Inventory playerInventory, int y) {
+        int j;
+        int k;
+        for(j = 0; j < 3; ++j) {
+            for(k = 0; k < 9; ++k) {
+                this.addSlot(new Slot(playerInventory, k + j * 9 + 9, 11 + k * 18, (84 + j * 18) + y));
+            }
+        }
     }
 }
